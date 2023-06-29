a1af577cdd5ad16d73e02ea459895c52f662940c
Rafail1 <ya.rafon-92@ya.ru>
2023-06-29 13:48:05 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'travos-mucho';
+export class AppComponent implements OnInit {
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
+
+  ngOnInit(): void {
+    const ctx = this.canvas.nativeElement.getContext('2d');
+    if (!ctx) {
+      throw Error('no canvas found');
+    }
+
+    this.ctx = ctx;
+    this.animate()
+  }
+
+  animate(): void {
+    this.ctx.fillStyle = 'green';
+    this.ctx.fillRect(10, 10, 150, 100);
+  }
 }
