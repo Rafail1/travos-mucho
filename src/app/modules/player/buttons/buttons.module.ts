4ce2a5328af5acdf32bfe40acf088bf48bb298a3
Rafail1 <ya.rafon-92@ya.ru>
2023-10-01 15:17:38 +0300

    player view

--- src/app/modules/player/buttons/buttons.module.ts
+++ src/app/modules/player/buttons/buttons.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
+import { NgIconsModule } from '@ng-icons/core';
+import { matPlayCircleOutline } from '@ng-icons/material-icons/outline';
+import {
+  matFastForwardSharp,
+  matFastRewindSharp,
+} from '@ng-icons/material-icons/sharp';
 import { ButtonsComponent } from './buttons.component';
 import { ButtonsService } from './buttons.service';
-
 @NgModule({
+  imports: [
+    NgIconsModule.withIcons({
+      matPlayCircleOutline,
+      matFastRewindSharp,
+      matFastForwardSharp,
+    }),
+  ],
   declarations: [ButtonsComponent],
   providers: [ButtonsService],
   exports: [ButtonsComponent],
