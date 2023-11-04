3b6d0b6720b6237c0e96686adfff7d13f3eed6e4
Rafail1 <ya.rafon-92@ya.ru>
2023-11-04 18:51:26 +0300

    circles

--- src/app/modules/scalp/renderer/d4/tick/tick-renderer.service.ts
+++ src/app/modules/scalp/renderer/d4/tick/tick-renderer.service.ts
@@ -2,26 +2,33 @@ import { Injectable } from '@angular/core';
 import { Selection } from 'd3';
 import { IAggTrade } from 'src/app/modules/backend/backend.service';
 import { GridService } from '../grid/grid.service';
-import { ConfigService } from 'src/app/config/config';
+import { ConfigService, STYLE_THEME_KEY } from 'src/app/config/config';
 const MAX_LENGTH = 100;
+const RADIUS = 10;
 @Injectable()
 export class TickRendererService {
   private svg: Selection<SVGSVGElement, unknown, null, undefined>;
   private circlesIndexes: Array<
     Selection<SVGGElement, unknown, null, undefined>
   > = [];
+  private askColor: string;
+  private bidColor: string;
 
   constructor(
     private gridService: GridService,
     private configService: ConfigService
-  ) {}
+  ) {
+    this.askColor = configService.getConfig(STYLE_THEME_KEY).fillAskColor;
+    this.bidColor = configService.getConfig(STYLE_THEME_KEY).fillBidColor;
+  }
 
   setSvg(svg: Selection<SVGSVGElement, unknown, null, undefined>) {
     this.svg = svg;
     for (let i = 0; i < MAX_LENGTH; i++) {
       const group = this.svg.insert('g');
       this.circlesIndexes.push(group);
-      group.insert('circle');
+      group.insert('circle').attr('cx', RADIUS).attr('cy', RADIUS);
+      group.insert('text');
     }
   }
 
@@ -42,7 +49,17 @@ export class TickRendererService {
     if (!group) {
       throw Error('circle not found');
     }
-    group.select('circle').attr('y', y);
+
+    this.circlesIndexes.forEach((item, idx) => {
+      group.select('circle').attr('x', (idx + 1) * RADIUS);
+    });
+
+    group
+      .select('circle')
+      .attr('y', y)
+      .attr('x', RADIUS)
+      .attr('fill', data.m ? this.askColor : this.bidColor);
+    group.select('text').text(data.q);
     this.circlesIndexes.unshift(group);
   }
 }
