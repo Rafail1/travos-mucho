90fdfdbc1d21c7da8286ed50f265f86d5d2b18f8
Rafail1 <ya.rafon-92@ya.ru>
2023-11-05 12:35:03 +0300

    perfomance

--- src/app/modules/scalp/renderer/d4/tick/tick-renderer.service.ts
+++ src/app/modules/scalp/renderer/d4/tick/tick-renderer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Selection } from 'd3';
+import { Selection, merge } from 'd3';
 import { IAggTrade } from 'src/app/modules/backend/backend.service';
 import { GridService } from '../grid/grid.service';
 import { ConfigService, STYLE_THEME_KEY } from 'src/app/config/config';
@@ -25,37 +25,59 @@ export class TickRendererService {
 
   setSvg(svg: Selection<SVGSVGElement, unknown, null, undefined>) {
     this.svg = svg;
+
     this.initCircles();
   }
 
   initCircles() {
     this.data = [];
-    for (let i = 0; i < MAX_LENGTH; i++) {
-      this.data.push({ y: 0, x: i * RADIUS, color: 'red' });
-    }
   }
   renderCircles() {
-    this.svg
+    const groups: any = this.svg
       .selectAll('g')
-      .data(this.data)
-      .enter()
-      .append('g')
-      .selectAll('circle')
-      .data<any>((d: any) => {
-        return [d];
-      })
-      .enter()
-      .append('circle')
-      .attr('r', RADIUS)
-      .attr('cx', function (d: any) {
-        return d.x;
-      })
-      .attr('cy', (d: any) => {
-        return d.y;
-      })
-      .attr('fill', function (d: any) {
-        return d.color;
+      .data(this.data, function (d: any) {
+        return d[0];
       });
+    const g = groups.join(function (enter: any) {
+      enter.append('g');
+    });
+    groups.exit().remove();
+    const circles: any = g
+      .selectAll('circle')
+      .data((d: any) => [d.slice(1, 4)]);
+    const texts: any = g.selectAll('text').data((d: any) => [d[4]]);
+
+    circles.join(
+      (enter: any) => {
+        enter.append('circle');
+      },
+      (update: any) => {
+        update
+          .attr('r', RADIUS)
+          .attr('cx', function (d: any) {
+            console.log(d);
+            return d[0];
+          })
+          .attr('cy', (d: any) => {
+            console.log(d);
+            return d[1];
+          })
+          .attr('fill', function (d: any) {
+            return d[2];
+          });
+      },
+      (exit: any) => {
+        exit.remove();
+      }
+    );
+    texts.join(
+      (enter: any) => {
+        enter.append('text');
+      },
+      (update: any) => {
+        update.text((d: any) => d);
+      }
+    );
   }
 
   clean() {
@@ -73,27 +95,20 @@ export class TickRendererService {
       return;
     }
 
-    this.data[this.latestElementIdx].y = y;
-    this.data[this.latestElementIdx].color = data.m
+    this.data[this.latestElementIdx][0] = this.latestElementIdx;
+    this.data[this.latestElementIdx][1] = y;
+    this.data[this.latestElementIdx][2] = this.latestElementIdx * RADIUS;
+    this.data[this.latestElementIdx][3] = data.m
       ? this.askColor
       : this.bidColor;
-    // const group = this.circlesIndexes[this.latestElementIdx];
-    // if (!group) {
-    //   throw Error('circle group not found');
-    // }
+    this.data[this.latestElementIdx][4] = data.q;
 
-    // this.circlesIndexes.forEach((item, idx) => {
-    //   item.attr('transform', `translate(${(idx + 1) * RADIUS})`);
-    // });
-
-    // group.select('circle').attr('fill', data.m ? this.askColor : this.bidColor);
-    // group.attr('transform', `translate(${RADIUS}, ${y})`);
-    // group.select('text').text(data.q);
     if (this.latestElementIdx === MAX_LENGTH - 1) {
       this.latestElementIdx = 0;
     } else {
       this.latestElementIdx++;
     }
+
     this.renderCircles();
   }
 }
