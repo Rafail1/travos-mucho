036d499480aaadfb8ec9c14740e24d897ed0edab
Rafail1 <ya.rafon-92@ya.ru>
2024-01-11 12:47:59 +0300

    proxy

--- rest/binance/exchange-info.php
+++
@@ -1,8 +0,0 @@
-<?
-$jsonurl = 'https://fapi.binance.com/fapi/v1/exchangeInfo';
-$json = file_get_contents($jsonurl);
-header('Content-type: application/json');
-header('Access-Control-Allow-Origin: *');
-echo $json;
-http_response_code(200);
-exit;
